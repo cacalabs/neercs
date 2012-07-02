279af8bcb224587e941788bcdb5fc2e342434593
Sam Hocevar <sam@hocevar.net>
2012-07-02 06:54:52 +0000

    data: fix a lot of paths to assets.

--- neercs/video/text-render.cpp
+++ neercs/video/text-render.cpp
@@ -43,7 +43,7 @@ TextRender::TextRender(caca_canvas_t *caca, ivec2 font_size)
 
 void TextRender::Init()
 {
-    m_font = new TileSet("neercs/video/resource/map.png",
+    m_font = new TileSet("tools/neercs/video/resource/map.png",
                          ivec2(256, 256), ivec2(1));
 
     m_shader = Shader::Create(lolfx_text);
