8915c4b6854d0dc6eca120b5d106a85da5a83f98
Sam Hocevar <sam@hocevar.net>
2015-04-27 23:21:39 +0000

    build: fix license and copyright information.

--- neercs/old/mini-neercs.h
+++ neercs/old/mini-neercs.h
@@ -1,15 +1,15 @@
-/*
- *  neercs        console-based window manager
- *  Copyright (c) 2006-2010 Sam Hocevar <sam@hocevar.net>
- *                2008-2010 Jean-Yves Lamoureux <jylam@lnxscene.org>
- *                2008-2010 Pascal Terjan <pterjan@linuxfr.org>
- *                All Rights Reserved
+﻿/*
+ *  neercs — console-based window manager
+ *
+ *  Copyright © 2006—2015 Sam Hocevar <sam@hocevar.net>
+ *            © 2008—2010 Jean-Yves Lamoureux <jylam@lnxscene.org>
+ *            © 2008—2010 Pascal Terjan <pterjan@linuxfr.org>
  *
  *  This program is free software. It comes without any warranty, to
  *  the extent permitted by applicable law. You can redistribute it
- *  and/or modify it under the terms of the Do What The Fuck You Want
- *  To Public License, Version 2, as published by Sam Hocevar. See
- *  http://www.wtfpl.net/ for more details.
+ *  and/or modify it under the terms of the Do What the Fuck You Want
+ *  to Public License, Version 2, as published by the WTFPL Task Force.
+ *  See http://www.wtfpl.net/ for more details.
  */
 
 void client_init(void);
--- neercs/old/python/py_module.h
+++ neercs/old/python/py_module.h
@@ -1,13 +1,13 @@
-/*
- *  neercs        console-based window manager
- *  Copyright (c) 2009-2010 Jean-Yves Lamoureux <jylam@lnxscene.org>
- *                All Rights Reserved
+﻿/*
+ *  neercs — console-based window manager
+ *
+ *  Copyright © 2009—2010 Jean-Yves Lamoureux <jylam@lnxscene.org>
  *
  *  This program is free software. It comes without any warranty, to
  *  the extent permitted by applicable law. You can redistribute it
- *  and/or modify it under the terms of the Do What The Fuck You Want
- *  To Public License, Version 2, as published by Sam Hocevar. See
- *  http://www.wtfpl.net/ for more details.
+ *  and/or modify it under the terms of the Do What the Fuck You Want
+ *  to Public License, Version 2, as published by the WTFPL Task Force.
+ *  See http://www.wtfpl.net/ for more details.
  */
 
 #ifdef USE_PYTHON
